490a100522f652586fda2cc20a8bdf7a19d20f32
mrmars20 <43221807+mrmars20@users.noreply.github.com>
2018-09-30 14:06:11 +0300

    task5

--- src/index.js
+++ src/index.js
@@ -2,4 +2,25 @@
 module.exports = function makeExchange(currency) {
     // Your code goes here!
     // Return an object containing the minimum number of coins needed to make change
+    if (currency > 10000){
+        return  {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    }
+    if (currency <= 0){
+        return {};
+    }
+    coins = {};
+    usedCoins = {"H": 50, "Q": 25, "D": 10, "N": 5, "P": 1};
+
+    while (currency > 0) {
+        for (var key in usedCoins) {
+            if (usedCoins[key] <= currency) {
+                coins[key] = coins[key] || 0;
+                coins[key]++;
+                currency -= usedCoins[key];
+                break;
+            }
+        }
+    }
+    return coins;
 }
+
